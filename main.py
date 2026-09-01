85f733865a49b22cf2240c9308ce350136ce9777
Rakesh <rakesh.717.02@gmail.com>
2026-09-01 12:20:41 +0530

    loaded all csv files to database

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import pandas as pd
 from src.load import get_db_connection, load_to_sqlite
+from src.extract_csv import csv_data
+
+customers_df = csv_data["customers"]
+
+conn = get_db_connection()
+load_to_sqlite(customers_df, "customers", conn)
+conn.close()
+
+conn = get_db_connection()
+for table_name, df in csv_data.items():
+    load_to_sqlite(df, table_name, conn)
+conn.close()
+
+conn = get_db_connection()
+tables = pd.read_sql(
+    "SELECT name FROM sqlite_master WHERE type='table';",
+    conn
+)
+print(tables)
+conn.close()
 
 conn = get_db_connection()
 
@@ -8,6 +28,11 @@ tables = pd.read_sql(
     conn
 )
 
-print(tables)
+for table in tables["name"]:
+    count = pd.read_sql(
+        f"SELECT COUNT(*) AS row_count FROM {table};",
+        conn
+    )
+    print(f"{table}: {count.iloc[0]['row_count']} rows")
 
 conn.close()
