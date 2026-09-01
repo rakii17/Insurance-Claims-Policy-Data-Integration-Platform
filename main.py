f7226c34ff5cce362233909ca4d5aefac93dac58
Rakesh <rakesh.717.02@gmail.com>
2026-09-01 12:42:54 +0530

    loaded excel data into target.db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.load import get_db_connection, load_to_sqlite
 from src.extract_csv import csv_data
+from src.extract_excel import excel_data
 
 customers_df = csv_data["customers"]
 
@@ -36,3 +37,19 @@ for table in tables["name"]:
     print(f"{table}: {count.iloc[0]['row_count']} rows")
 
 conn.close()
+
+branches_df = excel_data["branches"]
+
+conn = get_db_connection()
+load_to_sqlite(branches_df, "branches", conn)
+conn.close()
+print(branches_df.head())
+
+conn = get_db_connection()
+count = pd.read_sql(
+    "SELECT COUNT(*) AS row_count FROM branches;",
+    conn
+)
+print(f"branches: {count.iloc[0]['row_count']} rows")
+conn.close()
+print(branches_df.shape)
