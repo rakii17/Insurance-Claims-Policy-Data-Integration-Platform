4d46e0800fa117f611166d366d70a3e150f7680f
Rakesh <rakesh.717.02@gmail.com>
2026-09-01 13:13:16 +0530

    load database data to target.db

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import pandas as pd
 from src.load import get_db_connection, load_to_sqlite
 from src.extract_csv import csv_data
 from src.extract_excel import excel_data
+from src.extract_database import (policy_transactions_df, premium_transactions_df, claim_transactions_df)
 
-customers_df = csv_data["customers"]
-
-conn = get_db_connection()
-load_to_sqlite(customers_df, "customers", conn)
-conn.close()
-
+#csv connections
 conn = get_db_connection()
 for table_name, df in csv_data.items():
     load_to_sqlite(df, table_name, conn)
 conn.close()
 
+#target.db connections
 conn = get_db_connection()
 tables = pd.read_sql(
     "SELECT name FROM sqlite_master WHERE type='table';",
@@ -36,6 +33,7 @@ for table in tables["name"]:
     print(f"{table}: {count.iloc[0]['row_count']} rows")
 conn.close()
 
+#excel connections
 branches_df = excel_data["branches"]
 
 conn = get_db_connection()
@@ -51,3 +49,22 @@ count = pd.read_sql(
 print(f"branches: {count.iloc[0]['row_count']} rows")
 conn.close()
 print(branches_df.shape)
+
+#source_system.db conenctions
+conn = get_db_connection()
+load_to_sqlite(
+    policy_transactions_df,
+    "policy_transactions",
+    conn
+)
+load_to_sqlite(
+    premium_transactions_df,
+    "premium_transactions",
+    conn
+)
+load_to_sqlite(
+    claim_transactions_df,
+    "claim_transactions",
+    conn
+)
+conn.close()
